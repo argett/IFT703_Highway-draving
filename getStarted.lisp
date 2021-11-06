cb140359cd718cfe9c5c3d99ccfe7665fc433061
Lilian <Lilak.de.braize@gmail.com>
2021-11-06 13:07:35 -0400

    Setup code for presentation 2

--- getStarted.lisp
+++ getStarted.lisp
@@ -7,24 +7,45 @@
 ;; Add Chunk-types here
 
 (chunk-type my_car weight positionX positionY speed)
+(chunk-type turn X_relative_position)
+(chunk-type brake power)
 (chunk-type other_car positionX positionY relative_speed)
 (chunk-type accident positionX positionY relative_speed)
-(chunk-type turn X_relative_position)
 
 ;; Add Chunks here
 
 (add-dm
+ (voiture isa my_car weight w positionX x positionY y speed s)
+
  (goal isa accident positionX nil positionY 0 relative_speed nil)
 
- (brakeSoft isa my_car weight nil positionX nil positionY nil speed -2)
- (brakeHard isa my_car weight nil positionX nil positionY nil speed -5)
+ (brakeSoft isa brake power 2)
+ (brakeHard isa brake power 5)
 
- (turnR isa my_car positionX 1 positionY nil relative_speed nil)
- (turnL isa my_car positionX -1 positionY nil relative_speed nil)
+ (turnR isa turn X_relative_position 1) 
+ (turnL isa turn X_relative_position -1)
  )
 
  
 ;; Add productions here
+
+(p turnL
+  =goal>
+    ISA turn
+    X_relative_position -1
+    X_relative_position =deviation
+   =retrieval>
+      ISA         my_car
+      positionX   =x_pos_car
+==>
+  =goal>
+    ISA my_car
+    positionX     =x_pos_car + deviation
+)
+
+
+
+
 
 (p brake
    =goal>
@@ -50,27 +71,6 @@
 (p brake
   =goal>
     ISA brakeEasy or brakeHard ; peu importe le chunk on veut freiners, est-cepossible de mettre 'OR' ?
-    one-ans busy
-    one1 =num1
-    one2 =num2
-  =retrieval>
-    ISA addition-fact
-    addend1 =num1
-    addend2 =num2
-    sum =sum
-==>
-  =goal>
-    one-ans =sum
-    carry busy
-  +retrieval>
-    ISA addition-fact
-    addend1 10
-    sum =sum
-)
-
-(p turn
-  =goal>
-    ISA ; peu importe le chunk on veut freiners, est-cepossible de mettre 'OR' ?
     one-ans busy
     one1 =num1
     one2 =num2
